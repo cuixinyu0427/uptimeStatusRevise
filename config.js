a1041477fec14eaef1fb3eb7cb38b44b3a1c14b7
cuixinyu <70997227+cuixinyu0427@users.noreply.github.com>
2020-10-18 22:47:41 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,15 +2,15 @@
 window.Config = {
 
   // 站点名
-  SiteName: '凛空の小屋 服务器状态监控',
+  SiteName: 'cxyの小屋 服务器状态监控',
 
   // 站点链接
-  SiteUrl: '/',
+  SiteUrl: 'https://cuixinyu.icu/',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
-    'ur1004328-7deb9fc4df464fb1d9674772',
+    'u1056235-54a7d268474505d4d5ac90ba',
   ],
 
   // 是否显示监测站点的链接
@@ -24,12 +24,12 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: '晓空blog',
-      url: 'https://blog.moeworld.tech/'
+      text: 'cxyBlog',
+      url: 'https://cuixinyu.icu/'
     },
     {
       text: '关于',
-      url: 'http://about.rcraft.ml'
+      url: 'https://cuixinyu.icu/me/'
     }
   ]
 };
